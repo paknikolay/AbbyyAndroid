88177411fceeed9f5f5cf81b8c7fd25a7519c1ae
nikolai <pak_kolya98@mail.ru>
2019-11-13 12:14:38 +0300

    испарвил замечания

--- hw3/src/FrequencyCounter.java
+++ hw3/src/FrequencyCounter.java
@@ -1,20 +1,21 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class FrequencyCounter {
     public static void main(String[] args) {
-        HashMap<Character, Integer> frequency = new HashMap<Character, Integer>();
+        Map<Character, Integer> frequency = new HashMap<Character, Integer>();
 
-        try (FileInputStream inputStream = new FileInputStream("./text.txt")) {
+        try (Reader fileReader = new FileReader("./text.txt")) {
+            Reader inputStream = new BufferedReader(fileReader);
+            char[] input = new char[1];
 
             while (true) {
-                int input = inputStream.read();
-                if (input == -1) {
+                int res = inputStream.read(input);
+                char symbol = input[0];
+                if (res == -1) {
                     break;
                 }
-                char symbol = (char) input;
                 if (frequency.containsKey(symbol)) {
                     Integer value = Integer.sum(frequency.get(symbol), 1);
                     frequency.put(symbol, value);
@@ -29,9 +30,12 @@ public class FrequencyCounter {
             e.printStackTrace();
         }
 
-
-        for (HashMap.Entry<Character, Integer> entry : frequency.entrySet()) {
-            System.out.println("" + entry.getKey() + ": " + entry.getValue());
+        try (Writer fileWriter = new FileWriter("./out.txt")) {
+            for (Map.Entry<Character, Integer> entry : frequency.entrySet()) {
+                fileWriter.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
